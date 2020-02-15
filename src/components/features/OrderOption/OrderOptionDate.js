e253cb73c09f6169debacff14892c20958917bc4
Piotr Grzybowski <grzybek6@gmail.com>
2020-02-15 14:50:25 +0100

    update date picker

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DatePicker, { getDefaultLocale, registerLocale, setDefaultLocale } from 'react-datepicker';
+import DatePicker, { registerLocale, setDefaultLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-// eslint-disable-next-line no-unused-vars
+
 import gb from 'date-fns/locale/en-GB';
+import { setHours } from 'date-fns/';
 
 registerLocale('gb', gb);
 setDefaultLocale(gb);
 
-
+// const addhours = (date) => {
+// const hoursAmount = date.getTimezoneOffset() / 60;
+// console.log(hoursAmount, date, subHours(date, hoursAmount));
+// const newDate = subHours(date, hoursAmount);
+// return setHours(date, 12);
+// };
 // eslint-disable-next-line react/prop-types
 const OrderOptionDate = ({currentValue, setOptionValue, placeholder}) => (
-  <DatePicker selected={currentValue}
+  <DatePicker
+    selected={currentValue}
     placeholderText={placeholder}
     dateFormat="yyyy/MM/dd"
-    value={currentValue}
     locale={gb}
     inline
-    onChange={date => {console.log(date, getDefaultLocale()); setOptionValue(date);}} />
+    value={new Date(currentValue)}
+    onChange={date => {console.log(date);date = setHours(date, 12); setOptionValue(date);}} />
 );
 
 OrderOptionDate.propTypes = {
