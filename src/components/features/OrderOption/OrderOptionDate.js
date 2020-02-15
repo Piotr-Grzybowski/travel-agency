19b8537044308e180db76da1bb034aaeb9b5646f
Piotr Grzybowski <grzybek6@gmail.com>
2020-02-15 13:15:32 +0100

    update date picker

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DatePicker from 'react-datepicker';
+import DatePicker, { getDefaultLocale, registerLocale, setDefaultLocale } from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+// eslint-disable-next-line no-unused-vars
+import gb from 'date-fns/locale/en-GB';
 
+registerLocale('gb', gb);
+setDefaultLocale(gb);
+
+
+// eslint-disable-next-line react/prop-types
 const OrderOptionDate = ({currentValue, setOptionValue, placeholder}) => (
-  <DatePicker selected={currentValue} placeholderText={placeholder} dateFormat="yyyy/MM/dd" value={currentValue} onSelect={date => setOptionValue(date)}>
-  </DatePicker>
+  <DatePicker selected={currentValue}
+    placeholderText={placeholder}
+    dateFormat="yyyy/MM/dd"
+    value={currentValue}
+    locale={gb}
+    inline
+    onChange={date => {console.log(date, getDefaultLocale()); setOptionValue(date);}} />
 );
 
 OrderOptionDate.propTypes = {
   type: PropTypes.string,
-  currentValue: PropTypes.string,
   setOptionValue: PropTypes.func,
   placeholder: PropTypes.string,
 };
